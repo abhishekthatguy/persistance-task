57560f13da1611cc7a61644710f192c9202e5a04
@baba_coder <kashyapabhi688@gmail.com>
2021-02-09 14:16:54 +0530

    Problem solved code

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import CustomerList from './components/customer-list/index.js';
 import 'h8k-components';
 
+
 const title = "Customer List";
 
 function App() {
--- src/components/customer-list/index.js
+++ src/components/customer-list/index.js
@@ -1,16 +1,42 @@
-import React  from "react";
+import React,{useState}  from "react";
 import "./index.css";
-
+const customers=[{
+    id:1,
+    name:"Avii",
+    isActive:true
+  }]
 function CustomerList() {
+  const [CustomerList, setCustomerList]=useState(customers);
+  const [name, setName]=useState("");
+  const HandleChange=(e)=>{
+    setName(e.target.value);
+  }
+const AddCustomers=()=>{
+let input=name;
+if(input && input !==""){
+  setCustomerList(CustomerList.concat({
+    id:CustomerList.length +1,
+    name:input,
+    isActive:true
+  }))
+}else{
+  alert("Please Provide Proper Data....");
+}
+    alert("submit");
+  }
   return (
     <div className="mt-75 layout-column justify-content-center align-items-center">
       <section className="layout-row align-items-center justify-content-center">
-        <input type="text" className="large" placeholder="Name" data-testid="app-input"/>
-        <button type="submit" className="ml-30" data-testid="submit-button">Add Customer</button>
+        <input type="text" className="large" placeholder="Name" data-testid="app-input" onChange={HandleChange} value={name}/>
+        <button type="submit" className="ml-30" data-testid="submit-button" onClick={AddCustomers}>Add Customer</button>
       </section>
 
       <ul className="styled mt-50" data-testid="customer-list">
-          <li className="slide-up-fade-in" data-testid="list-item1" key="list-item1" >Customer</li>
+          {
+  CustomerList && CustomerList.length>0 && CustomerList.map((x,index)=>{
+    return <li className="slide-up-fade-in" data-testid="list-item1" key={x.id} >{x.name}</li>
+  })
+}
       </ul>
     </div>
   );
